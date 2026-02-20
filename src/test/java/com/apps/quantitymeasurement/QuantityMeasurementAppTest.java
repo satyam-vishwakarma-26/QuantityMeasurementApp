1977e9fc24980f8e9b1659f0a9606dd716548411
satyam-vishwakarma-26 <satyamsharma000000s@gmail.com>
2026-02-20 22:35:01 +0530

    [Satyam] : Added, feature UC2-InchEquality Test

--- src/test/java/com/apps/quantitymeasurement/QuantityMeasurementAppTest.java
+++ src/test/java/com/apps/quantitymeasurement/QuantityMeasurementAppTest.java
@@ -4,9 +4,13 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 import com.apps.quantitymeasurement.QuantityMeasurementApp.Feet;
+import com.apps.quantitymeasurement.QuantityMeasurementApp.Inches;
 
 public class QuantityMeasurementAppTest {
 
+	 /* ==============================
+    FEET TEST CASES (UC1)
+    ============================== */
     @Test
     void testFeetEquality_SameValue() {
         Feet f1 = new Feet(1.0);
@@ -43,5 +47,50 @@ public class QuantityMeasurementAppTest {
         Feet f1 = new Feet(1.0);
 
         assertTrue(f1.equals(f1), "Object should be equal to itself (reflexive property)");
+    }
+    
+    /* ==============================
+    INCHES TEST CASES (UC2)
+    ============================== */
+
+    @Test
+    void testInchesEquality_SameValue() {
+        Inches i1 = new Inches(12.0);
+        Inches i2 = new Inches(12.0);
+
+        assertTrue(i1.equals(i2), "12 in should be equal to 12 in");
+    }
+
+    @Test
+    void testInchesEquality_DifferentValue() {
+        Inches i1 = new Inches(12.0);
+        Inches i2 = new Inches(10.0);
+
+        assertFalse(i1.equals(i2), "12 in should not be equal to 10 in");
+    }
+
+    @Test
+    void testInchesEquality_NullComparison() {
+        Inches i1 = new Inches(12.0);
+
+        assertFalse(i1.equals(null),
+                "Inches object should not be equal to null");
+    }
+
+    @Test
+    void testInchesEquality_DifferentClass() {
+        Inches i1 = new Inches(12.0);
+        Feet feet = new Feet(1.0);
+
+        assertFalse(i1.equals(feet),
+                "Inches object should not be equal to Feet object");
+    }
+
+    @Test
+    void testInchesEquality_SameReference() {
+        Inches i1 = new Inches(12.0);
+
+        assertTrue(i1.equals(i1),
+                "Object should be equal to itself");
     }
 }
